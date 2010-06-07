19b939ce754ee2e3611bd61c56e0c9f28f88415d
David FRANCOIS <david.francois@webflows.fr>
2010-06-07 16:38:34 +0200

    Integrate data merge with conversions

--- lib/officer.rb
+++ lib/officer.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'erb'
 require 'fileutils'
+require 'tmpdir'
 require 'zip/zip'
 require 'open_office/server'
 
@@ -24,20 +25,36 @@ module Officer
   end
 
   def self.merge_template(template, options = {})
-    tmp = Tempfile.new("officer")
-    
-    tmp.write(merge(get_contents(template), :locals => options[:locals]))
-    tmp.close
+    # Get template contents
+    tmp_contents= Tempfile.new("officer-contents")
+    tmp_contents.write(merge(get_contents(template), :locals => options[:locals]))
+    tmp_contents.close
 
-    FileUtils.cp(template, options[:to]) if options[:to]
-    destination = options[:to] || template
-      
-    Zip::ZipFile.open(destination) do |zip|
-      zip.replace("content.xml", tmp.path)
+    # Copy the template so we can merge the data into the copy
+    tmp_merged_template = File.join(Dir.tmpdir, "merged-template-#{rand(10**9)}#{File.extname(template)}")
+    FileUtils.cp(template, tmp_merged_template)
+
+    # Stuff the merged contents.xml into the OpenDocument zip
+    Zip::ZipFile.open(tmp_merged_template) do |zip|
+      zip.replace("content.xml", tmp_contents.path)
       zip.commit
     end
 
-    tmp.unlink
+    # Remove the merged contents.xml
+    tmp_contents.unlink
+
+    # Manages the converted file depending on the context
+    if options[:to]
+      if File.extname(options[:to]) == File.extname(template)
+        FileUtils.mv(tmp_merged_template, options[:to])
+      else
+        OpenOffice::Server.convert(tmp_merged_template, options[:to])
+        FileUtils.rm(tmp_merged_template)
+      end
+    else
+      FileUtils.rm(template)
+      FileUtils.mv(tmp_merged_template, template)
+    end
   end
 
   def self.convert(from, to)
