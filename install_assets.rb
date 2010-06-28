894b50dc1c3cc9e238eea05394b4021bde6f2211
David FRANCOIS <david.francois@webflows.fr>
2010-06-28 21:40:17 +0200

    Copy configuration file at first Rails app startup

--- install_assets.rb
+++ install_assets.rb
@@ -1,20 +1,3 @@
-# Workaround a problem with script/plugin and http-based repos.
-# See http://dev.rubyonrails.org/ticket/8189
-Dir.chdir(Dir.getwd.sub(/vendor.*/, '')) do
-
-##
-## Copy over asset files (javascript/css/images) from the plugin directory to public/
-##
-
-  def copy_files(source_path, destination_path, directory, options={})
-    source = File.join(directory, source_path)
-    destination = File.join(Rails.root, destination_path)
-    FileUtils.mkdir(destination) unless File.exist?(destination)
-    FileUtils.cp_r(Dir.glob(source +'/*.*'), destination, options)
-  end
-  
-  unless File.exists?( File.join(Rails.root,"config/proselytism_config.yml"))
-    copy_files("/config", "/config", File.dirname(__FILE__))
-  end
-
+unless File.exists?(File.join(RAILS_ROOT , %w{config documentalist.yml}))
+  FileUtils.cp(File.join(File.dirname(__FILE__), %w{config documentalist.yml}), File.join(RAILS_ROOT, "config"))
 end
--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,2 @@
 require 'documentalist'
+require File.join(File.dirname(__FILE__), "..", "install_assets")
