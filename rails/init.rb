8d565e16f98081580a1373e71457c107f896a2da
David FRANCOIS <david.francois@webflows.fr>
2010-06-28 13:32:15 +0200

    For now it doesn't seem to be possible to automatically load rake tasks when embedded in a Rails app

--- rails/init.rb
+++ rails/init.rb
@@ -3,8 +3,6 @@ require 'documentalist'
 #Dir["#{Gem.searcher.find('facebooker').full_gem_path}/**/tasks/*.rake"].each { |ext| load ext }
 
 # Automatically load tasks when used in Rails
-Dir.glob(File.join(File.dirname(__FILE__), %w{ .. lib tasks ** *.rb})).each do |file|
-  load file
-end
-
-raise 'put'
+#Dir.glob(File.join(File.dirname(__FILE__), %w{ .. lib tasks ** *.rb})).each do |file|
+#  load file
+#end
