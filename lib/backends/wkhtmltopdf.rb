7c21e468391b212335a4ff0e523331492b3b78f4
David FRANCOIS <david.francois@webflows.fr>
2010-07-06 17:37:44 +0200

    Should extend possibility to pass arbitrary options to backend command lines

--- lib/backends/wkhtmltopdf.rb
+++ lib/backends/wkhtmltopdf.rb
@@ -5,7 +5,7 @@ module Documentalist
     depends_on_binaries! "wkhtmltopdf" => "install wkhtmltopdf package"
 
     def self.convert(file, options)
-      command = "wkhtmltopdf -q #{file} #{options[:to]}"
+      command = "wkhtmltopdf #{options[:backend_options] and options[:backend_options].join(" ")} #{file} #{options[:to]} > #{Documentalist.config[:log_file]} 2>&1"
       Documentalist.logger.debug("Going to run WkHtmlToPdf backend with command : #{command}")
       system(command)
     end
